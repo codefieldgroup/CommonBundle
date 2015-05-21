f2b21a596cbf558278433c1bdec008c60a1a0963
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-05-20 22:42:26 -0400

    Adicionando la posibilidad de hacer busquedas entre realiciones por ejemplo cfUser tiene muchos cfPerson y podemos decir en una busqueda dentro de cfUser:   [username => xxxx, cfPerson.firstname => xxxx, cfPerson.personalId => xxxx].
    esto es necesario cuando tenemos realiciones y deseamos buscar dentro de las relaciones que tenga la tabla de busqueda.

--- Entity/CommonRepository.php
+++ Entity/CommonRepository.php
@@ -9,6 +9,24 @@ class CommonRepository extends EntityRepository
 {
 
     /**
+     * Search with OR conditions.
+     *
+     * You can search intro relations, for ex:
+     * entity cfUser:
+     * username
+     * password
+     * cfPerson
+     *
+     * entity cfPerson:
+     * firstname
+     * lastname
+     * personalId
+     *
+     * and the query by criteria:
+     *
+     * $criteria = [ 'username' => 'john', 'cfPerson.firstname' => 'john', 'cfPerson.personalId' => 'john' ];
+     *
+     *
      * @param array $criteria
      * @param $count
      * @param array $orderBy
@@ -19,16 +37,31 @@ class CommonRepository extends EntityRepository
      */
     public function searchOr( array $criteria, &$count, array $orderBy = null, $limit = null, $offset = null )
     {
-        $em = $this->getEntityManager();
-        //		$expr = $em->getExpressionBuilder();
+        $em   = $this->getEntityManager();
+        $expr = $em->getExpressionBuilder();
 
         //get Count
         $qb_count = $em->createQueryBuilder();
         $qb_count->select( 'COUNT(entity.id)' )->from( $this->getEntityName(), 'entity' );
         if (is_array( $criteria ) && count( $criteria ) > 0) {
+            $join_name = [ ];
             foreach ($criteria as $key => $value) {
-                $qb_count = $qb_count->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
-                $qb_count->setParameter( 'value'.$key, '%'.$value.'%' );
+                //there are join?
+                //Estoy haciendo que cuando se pase una busqueda ex cfpatient.patientId se percate que tiene que hacer un join.
+                $join = explode( '.', $key );
+                if (count( $join ) > 1) {
+                    //To verify that not exist key the same join.
+                    if ( ! array_key_exists( $join[0], $join_name )) {
+                        echo array_key_exists( $join[0], $join_name );
+                        $join_name[$join[0]] = $join[0];
+                        $qb_count->join( 'entity.'.$join[0], $join[0] );
+                    }
+                    $qb_count = $qb_count->orWhere( $join[0].'.'.$join[1].' LIKE '.':value'.$join[1] );
+                    $qb_count->setParameter( 'value'.$join[1], '%'.$value.'%' );
+                } else {
+                    $qb_count = $qb_count->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
+                    $qb_count->setParameter( 'value'.$key, '%'.$value.'%' );
+                }
             }
         }
         $count = $qb_count->getQuery()->getSingleScalarResult();
@@ -42,9 +75,24 @@ class CommonRepository extends EntityRepository
         $qb->select( 'entity' )->from( $this->getEntityName(), 'entity' );
 
         if (is_array( $criteria ) && count( $criteria ) > 0) {
+            $join_name = [ ];
             foreach ($criteria as $key => $value) {
-                $qb = $qb->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
-                $qb->setParameter( 'value'.$key, '%'.$value.'%' );
+                //there are join?
+                //Estoy haciendo que cuando se pase una busqueda ex cfpatient.patientId se percate que tiene que hacer un join.
+                $join = explode( '.', $key );
+                if (count( $join ) > 1) {
+                    //To verify that not exist key the same join.
+                    if ( ! array_key_exists( $join[0], $join_name )) {
+                        echo array_key_exists( $join[0], $join_name );
+                        $join_name[$join[0]] = $join[0];
+                        $qb->join( 'entity.'.$join[0], $join[0] );
+                    }
+                    $qb = $qb->orWhere( $join[0].'.'.$join[1].' LIKE '.':value'.$join[1] );
+                    $qb->setParameter( 'value'.$join[1], '%'.$value.'%' );
+                } else {
+                    $qb = $qb->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
+                    $qb->setParameter( 'value'.$key, '%'.$value.'%' );
+                }
             }
         }
         if ($orderBy !== null && $orderBy) {
@@ -62,6 +110,23 @@ class CommonRepository extends EntityRepository
     }
 
     /**
+     * Search with AND conditions.
+     *
+     * You can search intro relations, for ex:
+     * entity cfUser:
+     * username
+     * password
+     * cfPerson
+     *
+     * entity cfPerson:
+     * firstname
+     * lastname
+     * personalId
+     *
+     * and the query by criteria:
+     *
+     * $criteria = [ 'username' => 'john', 'cfPerson.firstname' => 'john', 'cfPerson.personalId' => 'john' ];
+     *
      * @param array $criteria
      * @param $count
      * @param array $orderBy
@@ -79,9 +144,24 @@ class CommonRepository extends EntityRepository
         $qb_count = $em->createQueryBuilder();
         $qb_count->select( 'COUNT(entity.id)' )->from( $this->getEntityName(), 'entity' );
         if (is_array( $criteria ) && count( $criteria ) > 0) {
+            $join_name = [ ];
             foreach ($criteria as $key => $value) {
-                $qb_count = $qb_count->andWhere( 'entity.'.$key.' LIKE '.':value'.$key );
-                $qb_count->setParameter( 'value'.$key, '%'.$value.'%' );
+                //there are join?
+                //Estoy haciendo que cuando se pase una busqueda ex cfpatient.patientId se percate que tiene que hacer un join.
+                $join = explode( '.', $key );
+                if (count( $join ) > 1) {
+                    //To verify that not exist key the same join.
+                    if ( ! array_key_exists( $join[0], $join_name )) {
+                        echo array_key_exists( $join[0], $join_name );
+                        $join_name[$join[0]] = $join[0];
+                        $qb_count->join( 'entity.'.$join[0], $join[0] );
+                    }
+                    $qb_count = $qb_count->orWhere( $join[0].'.'.$join[1].' LIKE '.':value'.$join[1] );
+                    $qb_count->setParameter( 'value'.$join[1], '%'.$value.'%' );
+                } else {
+                    $qb_count = $qb_count->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
+                    $qb_count->setParameter( 'value'.$key, '%'.$value.'%' );
+                }
             }
         }
         $count = $qb_count->getQuery()->getSingleScalarResult();
@@ -91,9 +171,24 @@ class CommonRepository extends EntityRepository
         $qb->select( 'entity' )->from( $this->getEntityName(), 'entity' );
 
         if (is_array( $criteria ) && count( $criteria ) > 0) {
+            $join_name = [ ];
             foreach ($criteria as $key => $value) {
-                $qb = $qb->andWhere( 'entity.'.$key.' LIKE '.':value'.$key );
-                $qb->setParameter( 'value'.$key, '%'.$value.'%' );
+                //there are join?
+                //Estoy haciendo que cuando se pase una busqueda ex cfpatient.patientId se percate que tiene que hacer un join.
+                $join = explode( '.', $key );
+                if (count( $join ) > 1) {
+                    //To verify that not exist key the same join.
+                    if ( ! array_key_exists( $join[0], $join_name )) {
+                        echo array_key_exists( $join[0], $join_name );
+                        $join_name[$join[0]] = $join[0];
+                        $qb->join( 'entity.'.$join[0], $join[0] );
+                    }
+                    $qb = $qb->orWhere( $join[0].'.'.$join[1].' LIKE '.':value'.$join[1] );
+                    $qb->setParameter( 'value'.$join[1], '%'.$value.'%' );
+                } else {
+                    $qb = $qb->orWhere( 'entity.'.$key.' LIKE '.':value'.$key );
+                    $qb->setParameter( 'value'.$key, '%'.$value.'%' );
+                }
             }
         }
         if ($orderBy !== null) {
