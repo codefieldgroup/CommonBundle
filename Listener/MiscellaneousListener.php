934eab5c716ae51054479cf41a93374b13e763c8
Jose Carlos Ramos Carmenates <jramoscarmenates@gmail.com>
2015-06-21 17:15:21 -0400

    Se le adicionó la funcionalidad de incluir el prepareAttributes al settear los datos al objeto, de esta forma quedaría todo en esta sola funcion y nos evita tener que ejecutar prepareAttribues antes de llamar a esta funcion, con tan solo decirle que prepareAttributes "true" el solo se encarga de hacerlo.
    Es recomendable solo usar el prepare en true cuando se esté seguro que exisiten relaciones entre tablas en el objeto que se esta bind.

--- Listener/MiscellaneousListener.php
+++ Listener/MiscellaneousListener.php
@@ -3,9 +3,21 @@ namespace Cf\CommonBundle\Listener;
 
 use Symfony\Component\Security\Core\SecurityContext;
 use \Doctrine\Common\Util\Inflector as Inflector;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class MiscellaneousListener
 {
+
+    private $container;
+
+    /**
+     * @param ContainerInterface $container
+     */
+    public function __construct( ContainerInterface $container )
+    {
+        $this->container = $container;
+    }
+
 
     /**
      *
@@ -58,27 +70,46 @@ class MiscellaneousListener
      *
      * @param $object
      * @param $arguments
+     * @param bool $prepareAttributes
      *
      * @return mixed
      */
-    static public function bindParameters( &$object, $arguments )
+
+    public function bindParameters( &$object, $arguments, $prepareAttributes = false)
     {
+        if ($prepareAttributes !== false){
+            $em = $this->container->get( 'doctrine.orm.entity_manager' );
+
+            $arguments = $em->getRepository(get_class($object))->prepareAttributes($arguments);
+        }
+
         if (is_array( $arguments ) && count( $arguments ) > 0) {
             foreach ($arguments as $property => $argument) {
                 $local_function = sprintf( 'set%s', Inflector::camelize( $property ) );
+                $get_local_function = sprintf( 'get%s', Inflector::camelize( $property ) );
                 if (method_exists( $object, $local_function )) {
+
+                    if(is_object($object->$get_local_function())){
+                        if( $object->$get_local_function() instanceof  \DateTime && !is_object($argument)){
+                            $argument = new \DateTime($argument);
+                        }
+                    }
                     $object->$local_function( $argument );
                     continue;
                 }
 
                 $local_function = sprintf( 'set%s', ucwords( $property ) );
                 if (method_exists( $object, $local_function )) {
+                    echo $local_function;
+
                     $object->$local_function( $argument );
                     continue;
                 }
 
                 $local_function = sprintf( 'set%s', $property );
                 if (method_exists( $object, $local_function )) {
+                    echo $local_function;
+
                     $object->$local_function( $argument );
                     continue;
                 }
